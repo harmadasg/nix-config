5e72c48a89462abb8e8e1bf17cbcb02d3dcce664
Gergely Harmadas <harmadasg@gmail.com>
2024-09-02 23:10:29 +0200

    Add Home Manager

--- configuration.nix
+++ configuration.nix
@@ -108,6 +108,11 @@
     git
   ];
 
+  # I use zsh btw
+  environment.shells = with pkgs; [ zsh ];
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
--- flake.nix
+++ flake.nix
@@ -4,16 +4,25 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
-      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
-      nixos = lib.nixosSystem {
-        system = "x86_64-linux";
+      nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
         modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      gege =  home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
       };
     };
   };
