49d1333cf333a4ebdcad4c324b442adb68fee5b3
Gergely Harmadas <harmadasg@gmail.com>
2024-10-17 22:52:53 +0200

    Add more Hyprland tooling

---
+++ wm/hyprland/hypridle.nix
@@ -0,0 +1,21 @@
+{ pkgs, inputs, ... }:
+
+{
+  home.packages = with pkgs; [
+    hypridle
+  ];
+
+  programs.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";  # avoid starting multiple hyprlock instances.
+      };
+
+      listener = {
+        timeout = 300;                            # 5min
+        on-timeout = "loginctl lock-session";     # lock screen when timeout has passed
+      };
+    };
+  };  
+}
