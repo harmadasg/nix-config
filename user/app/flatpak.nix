915dbe80459e32f890d0e5e43de9bda4eced41c2
Gergely Harmadas <harmadasg@gmail.com>
2024-11-22 00:16:10 +0100

    Add a few Flatpak apps

--- user/app/flatpak.nix
+++ user/app/flatpak.nix
@@ -9,6 +9,9 @@
     # Check if gamepads are not working
     # https://github.com/flathub/com.valvesoftware.Steam/wiki#my-controller-isnt-being-detected
     "com.valvesoftware.Steam"
+    "com.calibre_ebook.calibre"
+    "org.qbittorrent.qBittorrent"
+    "org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/23.08" # this branch supports Lutris integration
   ];
   
   # Used by gmodena/nix-flatpak for declarative management
