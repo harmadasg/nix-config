4dc590f553684ab9cf23b08a6ca8ed59c09e9e4a
Gergely Harmadas <harmadasg@gmail.com>
2024-12-08 20:37:42 +0100

    Enable bluetooth service

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -1,38 +1,40 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./../../system/hardware-configuration.nix
-      ./../../system/wm/sddm.nix
-      ./../../system/hardware/boot.nix
-      ./../../system/hardware/networking.nix
-      ./../../system/hardware/graphics.nix
-      ./../../system/hardware/sound.nix
-      ./../../system/misc/i18n.nix
-      ./../../system/misc/console.nix
-      ./../../system/security/sshd.nix
-      ./../../system/app/flatpak.nix
-      # ./../../system/wm//plasma.nix
-      ./../../system/wm/hyprland.nix
-    ];
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./../../system/hardware-configuration.nix
+    ./../../system/wm/sddm.nix
+    ./../../system/hardware/boot.nix
+    ./../../system/hardware/networking.nix
+    ./../../system/hardware/graphics.nix
+    ./../../system/hardware/sound.nix
+    ./../../system/hardware/bluetooth.nix
+    ./../../system/misc/i18n.nix
+    ./../../system/misc/console.nix
+    ./../../system/security/sshd.nix
+    ./../../system/app/flatpak.nix
+    # ./../../system/wm//plasma.nix
+    ./../../system/wm/hyprland.nix
+  ];
 
   # Enable the Flakes feature and the accompanying new nix command-line tool
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users = {
-      defaultUserShell = pkgs.zsh;
-      users = {
-        gege = {
-          isNormalUser = true;
-          initialPassword = "pw123";
-          extraGroups = [ "networkmanager" "input" "wheel" ]; # Enable ‘sudo’ for the user.
-        };
+    defaultUserShell = pkgs.zsh;
+    users = {
+      gege = {
+        isNormalUser = true;
+        initialPassword = "pw123";
+        extraGroups = ["networkmanager" "input" "wheel"]; # Enable ‘sudo’ for the user.
+      };
     };
   };
 
@@ -62,5 +64,4 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.05"; # Did you read the comment?
-
 }
