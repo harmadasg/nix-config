781f99e24326821b36973c3cabaf9c63abc77a7f
Gergely Harmadas <harmadasg@gmail.com>
2025-05-06 21:57:37 +0200

    Add nix-darwin config

---
+++ profiles/work/configuration.nix
@@ -0,0 +1,25 @@
+{pkgs, self, ...}: {
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    wget
+    tree
+  ];
+
+  # Auto upgrade nix package
+  # nix.package = pkgs.nix;
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true; # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 5;
+}
