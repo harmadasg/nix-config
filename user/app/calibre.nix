83ff50c06ac49392e3ce51f559c5d6f2d203b84c
Gergely Harmadas <harmadasg@gmail.com>
2026-03-09 20:49:55 +0100

    Update packages

--- user/app/calibre.nix
+++ user/app/calibre.nix
@@ -1,12 +1,6 @@
 {pkgs, ...}: {
-  # TODO remove this workaround
-  home.packages = [
-    (pkgs.calibre.overrideAttrs (old: {
-      installPhase = ''
-        export QMAKE="${pkgs.qt6.qtbase}/bin/qmake"
 
-        ${old.installPhase}
-      '';
-    }))
+  home.packages = [
+    pkgs.calibre
   ];
 }
